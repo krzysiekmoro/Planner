f33eaeb81a32d2339435dc3830d83d9ab51e29db
krzysiekmoro <krzysztof@DESKTOP-4LIJPN6.localdomain>
2020-04-19 18:04:35 +0200

    complete

---
+++ dist/precache-manifest.0c3f74934f96775273dbb06ec9460ec0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0df1cd3af47b04f521ef4c29fbc9dbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da98f043586d91c38a9c",
+    "url": "/static/css/main.c36b1180.chunk.css"
+  },
+  {
+    "revision": "d14fa2da471adffd9774",
+    "url": "/static/js/0.ddf51c6c.chunk.js"
+  },
+  {
+    "revision": "e0c828d83273aa858690",
+    "url": "/static/js/3.af7e4709.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/static/js/3.af7e4709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dcaedd6cfecf876c77c",
+    "url": "/static/js/4.7ac14f8e.chunk.js"
+  },
+  {
+    "revision": "8b4e405be05455e84caa",
+    "url": "/static/js/5.8a59ae65.chunk.js"
+  },
+  {
+    "revision": "2c43c87998123f45dae7",
+    "url": "/static/js/6.f26ad1b0.chunk.js"
+  },
+  {
+    "revision": "8f1a656d2e37c3477ebc",
+    "url": "/static/js/7.84c684fc.chunk.js"
+  },
+  {
+    "revision": "71b95acfcfa68a8fa7a7",
+    "url": "/static/js/8.5cd3b21a.chunk.js"
+  },
+  {
+    "revision": "ae0b63f6d8bee613bb7a",
+    "url": "/static/js/9.c8f40e01.chunk.js"
+  },
+  {
+    "revision": "da98f043586d91c38a9c",
+    "url": "/static/js/main.3711be21.chunk.js"
+  },
+  {
+    "revision": "b9faee84268846a89436",
+    "url": "/static/js/runtime-main.b8095608.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
-import Dashboard from './components/dashboard/Dashboard';
-import ProjectDetails from './components/projects/ProjectDetails';
-import SignIn from './components/auth/SignIn';
-import SignUp from './components/auth/SignUp';
-import CreateProject from './components/projects/CreateProject';
+import { useSelector } from 'react-redux';
+import { isLoaded } from 'react-redux-firebase';
 
-import { useSelector } from 'react-redux'
-import { isLoaded } from 'react-redux-firebase'
+const Dashboard = lazy(() => import('./components/dashboard/Dashboard'));
+const ProjectDetails = lazy(() => import('./components/projects/ProjectDetails'));
+const SignIn = lazy(() => import('./components/auth/SignIn'));
+const SignUp = lazy(() => import('./components/auth/SignUp'));
+const CreateProject = lazy(() => import('./components/projects/CreateProject'))
+const Navbar = lazy(() => import('./components/layout/Navbar'));
+
+
+
 
 function AuthIsLoaded({ children }) {
   const auth = useSelector(state => state.firebase.auth)
@@ -20,16 +23,16 @@ function App() {
   return (
     <BrowserRouter>
       <AuthIsLoaded>
-      <div className="App">
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={Dashboard} />
-          <Route path='/project/:id' component={ProjectDetails} />
-          <Route path='/signin' component={SignIn} />
-          <Route path='/signup' component={SignUp} />
-          <Route path='/create' component={CreateProject} />
-        </Switch>
-      </div>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Dashboard} />
+            <Route path='/project/:id' component={ProjectDetails} />
+            <Route path='/signin' component={SignIn} />
+            <Route path='/signup' component={SignUp} />
+            <Route path='/create' component={CreateProject} />
+          </Switch>
+        </Suspense>
       </AuthIsLoaded>
     </BrowserRouter>
   );
